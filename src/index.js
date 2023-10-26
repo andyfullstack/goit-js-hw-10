d2184ff5f79dbe158076aa096fa9b0016ec454c5
andyfullstack <andriikoliadenko@gmail.com>
2023-10-26 21:09:52 +0300

    test done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchBreeds, fetchCatByBreed } from './js/apikey';
+import { fetchBreeds, fetchCatByBreed } from '../js/apikey';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio.js';
